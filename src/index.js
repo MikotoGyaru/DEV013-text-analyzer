e48623d56529a694fdd4323863cc710105c5f5b9
MikotoGyaru <catalinealarcon@gmail.com>
2023-12-14 00:01:06 -0300

    Agregué el botón en javascript

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import analyzer from './analyzer.js';
-
+let deleteButton=document.createElement("button");
+deleteButton.textContent="Limpiar métricas";
+deleteButton.classList.add("delete-button");
+deleteButton.addEventListener("click",function() {
+    let textareaDocument=document.querySelector("#element-to-remove");
+    textareaDocument.value="";
+});
+    let containerElement=document.querySelector("#areadetexto");
+    containerElement.appendChild(deleteButton);
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
