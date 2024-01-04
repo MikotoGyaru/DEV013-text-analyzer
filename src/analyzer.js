4b5316bdd521433333fd2f86bde5decc8aafefbb
MikotoGyaru <catalinealarcon@gmail.com>
2024-01-04 01:27:27 -0300

    css

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,21 +1,38 @@
 const analyzer = {  
   getWordCount: (text) => {
+    //const words = text.split(/\s+/);
+    //return words.length;
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getCharacterCount: (text) => {
+    return text.length;
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
   },
   getCharacterCountExcludingSpaces: (text) => {
+    const cleanedText = text.replace(/[^\w\s]/g, '');
+    return cleanedText.length;
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) => { 
+    const words=text.split(/\s+/);
+    let wordAvg=0;
+    for (let i=0; i < words.length; i++) {
+      wordAvg+=words[i].length;
+    }
+    return wordAvg/words.length;
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberCount: (text) => {
+    const numbers = text.match(/\d+/g);
+    return numbers ? numbers.length : 0;
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberSum: (text) => {
+    const numbers=text.match(/\d+/g);
+    return numbers ? numbers.reduce((acc,num) => acc+parseInt(num), 0):0;
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    //Primero debe reconocer los números ingresados en el cuadro de texto
+    //
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
 import analyzer from './analyzer.js';
-let deleteButton=document.createElement("button");
-deleteButton.textContent="Limpiar métricas";
-deleteButton.classList.add("delete-button");
+//let deleteButton=document.createElement("button");
+//deleteButton.textContent="Limpiar métricas";
+//deleteButton.classList.add("delete-button");
+let deleteButton=document.getElementById("reset-button")
 deleteButton.addEventListener("click",function() {
-    let textareaDocument=document.querySelector("#element-to-remove");
+    let textareaDocument=document.getElementsByName("user-input")[0];
     textareaDocument.value="";
 });
-    let containerElement=document.querySelector("#areadetexto");
-    containerElement.appendChild(deleteButton);
+    //let containerElement=document.querySelector("#areadetexto");
+    //containerElement.appendChild(deleteButton);
+
+    let userInput = document.getElementsByName("user-input")[0];
+    userInput.addEventListener("input", function() {
+          // Este código se ejecutará cada vez que el contenido del input cambie
+          let textoIngresado = userInput.value;
+          console.log("Texto ingresado:", textoIngresado);
+          let wordCount=analyzer.getWordCount(textoIngresado);
+          console.log("cantidad-de-palabras: " + wordCount);
+          let characterCount=analyzer.getCharacterCount(textoIngresado);
+          console.log("cantidad-de-caracteres: " + characterCount);
+          let characterCountExcludingSpaces=analyzer.getCharacterCountExcludingSpaces(textoIngresado);
+          console.log("cantidad-de-caracteres-sin-espacio: "+characterCountExcludingSpaces);
+          let averageWordLength=analyzer.getAverageWordLength(textoIngresado);
+          console.log("longitud-promedio-de-palabras: "+averageWordLength);
+          let numberCount=analyzer.getNumberCount(textoIngresado);
+          console.log("cantidad-de-números: "+numberCount);
+          let numerSum=analyzer.getNumberSum(textoIngresado);
+          console.log("suma-números: "+numerSum);
+        });
+
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
